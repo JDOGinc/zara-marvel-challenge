40595f710ce10e9487ad5a1e39078a1d36fce71f
JD Ortega <jortega@techtitute.com>
2024-03-07 12:31:34 +0000

    change Api protocol and index title

--- src/apiConfig.ts
+++ src/apiConfig.ts
@@ -1,1 +1,2 @@
-export const getApiUrl = (path: string) => `http://gateway.marvel.com/v1/${path}?ts=1&hash=b8ee3adfc0bfbc04baa60910e1bd9892&apikey=b4f8ff2a93d02223e47baf2073145521`
+export const getApiUrl = (path: string) =>
+  `https://gateway.marvel.com/v1/${path}?ts=1&hash=b8ee3adfc0bfbc04baa60910e1bd9892&apikey=b4f8ff2a93d02223e47baf2073145521`
